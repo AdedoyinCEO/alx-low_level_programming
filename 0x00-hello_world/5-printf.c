521ced90655330542962a3e29a50f9f98c329ced
Adedoyin <benedictadedoyin@icloud.com>
2022-09-18 09:09:16 -0700

    a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - prints out a string via printf
+ * Description: Program that prints out the string s followed by a new line
+ * Return: int 0 when done and no error*
+ */
+
+int main(void)
+{
+
+	printf("with a proper grammar,but the outcome is a piece of art,/n");
+	return(0);
+	
+
+
+}
